116658183b584691f7fac4f3e9d948ee55626309
andyfong <fangkun1991s@163.com>
2018-10-15 16:47:30 +0800

    chore(libs): localize react-draggable in libs with ts/tsx support

--- webapp/libs/react-draggable/index.ts
+++ webapp/libs/react-draggable/index.ts
@@ -1,9 +1,15 @@
-var Draggable = require('./lib/Draggable').default;
+import Draggable from './lib/Draggable'
+import DraggableCore from './lib/DraggableCore'
 
 // Previous versions of this lib exported <Draggable> as the root export. As to not break
 // them, or TypeScript, we export *both* as the root and as 'default'.
 // See https://github.com/mzabriskie/react-draggable/pull/254
 // and https://github.com/mzabriskie/react-draggable/issues/266
-module.exports = Draggable;
-module.exports.default = Draggable;
-module.exports.DraggableCore = require('./lib/DraggableCore').default;
+
+console.log(Draggable, DraggableCore)
+
+export default Draggable
+export {
+  Draggable,
+  DraggableCore
+}
