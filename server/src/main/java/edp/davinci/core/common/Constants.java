4f3f7752394b295f415962dd68ae92d8166110c6
shanmengm <mengm.shan@gmail.com>
2018-10-31 18:42:22 +0800

    fix bug

--- server/src/main/java/edp/davinci/core/common/Constants.java
+++ server/src/main/java/edp/davinci/core/common/Constants.java
@@ -20,8 +20,6 @@ package edp.davinci.core.common;
 
 import edp.core.consts.Consts;
 
-import java.io.File;
-
 /**
  * 常量
  */
@@ -51,29 +49,29 @@ public class Constants extends Consts {
     /**
      * 用户头像上传地址
      */
-    public static final String USER_AVATAR_PATH = File.separator + "image" + File.separator + "user" + File.separator;
+    public static final String USER_AVATAR_PATH = "/image/user/";
 
     /**
      * 组织头像上传地址
      */
-    public static final String ORG_AVATAR_PATH = File.separator + "image" + File.separator + "organization" + File.separator;
+    public static final String ORG_AVATAR_PATH = "/image/organization/";
 
 
     /**
      * display封面图地址
      */
-    public static final String DISPLAY_AVATAR_PATH = File.separator + "image" + File.separator + "display" + File.separator;
+    public static final String DISPLAY_AVATAR_PATH = "/image/display/";
 
 
     /**
      * CSV地址
      */
-    public static final String SOURCE_CSV_PATH = File.separator + "source" + File.separator + "csv" + File.separator;
+    public static final String SOURCE_CSV_PATH = "/source/csv/";
 
     /**
      * 团队头像上传地址
      */
-    public static final String TEAM_AVATAR_PATH = File.separator + "image" + File.separator + "organization" + File.separator;
+    public static final String TEAM_AVATAR_PATH = "/image/organization/";
 
     /**
      * 邀请组织成员邮件主题
