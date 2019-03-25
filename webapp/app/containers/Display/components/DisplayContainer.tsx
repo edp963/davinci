01119bd44534ac503b1589760863568005cd9eea
andyfong <fangkun1991s@163.com>
2019-03-25 17:22:19 +0800

    refactor(Display): refactor Editor transform scale code

--- webapp/app/containers/Display/components/DisplayContainer.tsx
+++ webapp/app/containers/Display/components/DisplayContainer.tsx
@@ -46,10 +46,8 @@ interface IDisplayContainerProps {
 }
 
 interface IDisplayContainerStates {
+  translate: string
   scale: number
-  width: number
-  height: number
-  padding: string
 }
 
 export class DisplayContainer extends React.Component<IDisplayContainerProps, IDisplayContainerStates> {
@@ -62,10 +60,8 @@ export class DisplayContainer extends React.Component<IDisplayContainerProps, ID
   constructor (props: IDisplayContainerProps) {
     super(props)
     this.state = {
-      scale: 1,
-      width: 0,
-      height: 0,
-      padding: ''
+      translate: '',
+      scale: 1
     }
   }
 
@@ -96,54 +92,56 @@ export class DisplayContainer extends React.Component<IDisplayContainerProps, ID
   private updateStyle = (zoomRatio: number, slideParams: any, onScaleChange: (scale: number) => void) => {
     const { clientWidth, clientHeight } = this.container.current
     const [containerWidth, containerHeight] = [clientWidth, clientHeight].map((item) => Math.max(zoomRatio, 1) * item)
+    const { width: slideWidth, height: slideHeight } = slideParams
 
-    let scale = (slideParams.width / slideParams.height > containerWidth / containerHeight) ?
+    let scale = (slideWidth / slideHeight > containerWidth / containerHeight) ?
       // landscape
-      (containerWidth - 64) / slideParams.width * zoomRatio :
+      (containerWidth - 64) / slideWidth * zoomRatio :
       // portrait
-      (containerHeight - 64) / slideParams.height * zoomRatio
+      (containerHeight - 64) / slideHeight * zoomRatio
     scale = +(Math.floor(scale / 0.05) * 0.05).toFixed(2)
-
-    const leftRightPadding = Math.max((clientWidth - slideParams.width * scale) / 2, 32)
-    const topBottomPadding = Math.max((clientHeight - slideParams.height * scale) / 2, 32)
-    const nextStyle = {
-      width: containerWidth,
-      height: containerHeight,
-      padding: `${topBottomPadding}px ${leftRightPadding}px`,
-      scale
-    }
-    this.setState({ ...nextStyle })
-    onScaleChange(nextStyle.scale)
+    const translateX = (Math.max(clientWidth - slideWidth * scale, 64)) / (2 * slideWidth) * 100
+    const translateY = (Math.max(clientHeight - slideHeight * scale, 64)) / (2 * slideHeight) * 100
+    const translate = `translate(${translateX}%, ${translateY}%)`
+    this.setState({
+      scale,
+      translate
+    })
+    onScaleChange(scale)
   }
 
   public createCoverCut = () => {
     const { onCoverCutCreated } = this.props
-    const { scale } = this.state
-    this.content.current.style.transform = 'scale(1)'
+    const transformTemp = this.content.current.style.transform
+    this.content.current.style.transform = transformTemp.replace(/scale\([\.|\d]*\)/, 'scale(1)')
     // captureVideosWithImages()
-    html2canvas(this.content, { useCORS: true }).then((canvas) => {
-      this.content.current.style.transform = `scale(${scale})`
+    html2canvas(this.content.current, { useCORS: true }).then((canvas) => {
+      this.content.current.style.transform = transformTemp
       canvas.toBlob((blob) => {
         onCoverCutCreated(blob)
       })
     })
   }
 
-  private getSlideStyle = (slideParams, scale: number) => {
+  private getSlideStyle = (slideParams, scale: number, translate: string) => {
+    if (!this.container.current) { return null }
+
+    const { width: slideWidth, height: slideHeight } = slideParams
     let slideStyle: React.CSSProperties
     slideStyle  = {
-      width: `${slideParams.width}px`,
-      height: `${slideParams.height}px`,
-      transform: `scale(${scale})`,
-      backgroundSize: 'cover'
+      width: `${slideWidth}px`,
+      height: `${slideHeight}px`,
+      backgroundSize: 'cover',
+      transform: `${translate} scale(${scale})`
     }
 
-    if (slideParams.backgroundColor) {
-      const rgb = slideParams.backgroundColor.join()
+    const { backgroundColor, backgroundImage } = slideParams
+    if (backgroundColor) {
+      const rgb = backgroundColor.join()
       slideStyle.backgroundColor = `rgba(${rgb})`
     }
-    if (slideParams.backgroundImage) {
-      slideStyle.backgroundImage = `url("${slideParams.backgroundImage}")`
+    if (backgroundImage) {
+      slideStyle.backgroundImage = `url("${backgroundImage}")`
     }
 
     return slideStyle
@@ -208,33 +206,23 @@ export class DisplayContainer extends React.Component<IDisplayContainerProps, ID
       onLayersSelectionRemove
     } = this.props
 
-    const { width, height, padding, scale } = this.state
+    const { scale, translate } = this.state
 
-    const slideStyle = this.getSlideStyle(slideParams, scale)
+    const slideStyle = this.getSlideStyle(slideParams, scale, translate)
 
     return (
-      <div className={styles.editor}>
-        <div ref={this.container} className={styles.editorContainer}>
-          <div
-            className={styles.displayContainer}
-            style={{
-              width: `${width}px`,
-              height: `${height}px`,
-              padding
-            }}
-            tabIndex={0}
-          >
-            <div className={styles.displayPanelWrapper}>
-              <div
-                ref={this.content}
-                className={styles.displayPanel}
-                style={slideStyle}
-                onClick={onLayersSelectionRemove}
-              >
-                {children}
-              </div>
-            </div>
-          </div>
+      <div
+        ref={this.container}
+        className={styles.displayContainer}
+        tabIndex={0}
+      >
+        <div
+          ref={this.content}
+          className={styles.displayPanel}
+          style={slideStyle}
+          onClick={onLayersSelectionRemove}
+        >
+          {children}
         </div>
       </div>
     )
