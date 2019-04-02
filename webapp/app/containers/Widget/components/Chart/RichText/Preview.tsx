c01702414dd565fa8a4232c0796b52cfde194038
andyfong <fangkun1991s@163.com>
2019-04-02 13:32:53 +0800

    fix(RichText): fix richText field value format

--- webapp/app/containers/Widget/components/Chart/RichText/Preview.tsx
+++ webapp/app/containers/Widget/components/Chart/RichText/Preview.tsx
@@ -19,24 +19,30 @@
  */
 
 import React from 'react'
+import { getFormattedValue } from '../../util'
 const Styles = require('./RichText.less')
 
 interface IRichTextPreviewProps {
   content: string
   data: any[]
   fieldBoundaries: [string, string]
+  mapFields: object
 }
 
 export class RichTextPreview extends React.PureComponent<IRichTextPreviewProps> {
 
   private renderFormattedText = () => {
-    const { content, data, fieldBoundaries } = this.props
+    const { content, data, fieldBoundaries, mapFields } = this.props
     const [ prefix, suffix ] = fieldBoundaries
     const fieldRegx = new RegExp(`${prefix}(.+?)${suffix}`, 'g')
 
     const formattedText = content.replace(fieldRegx, (_, p1: string) => {
       if (!data.length || data[0][p1] === null) { return '' }
-      const text = data.map((item) => item[p1]).join(', ')
+      let text = data.map((item) => item[p1]).join(', ')
+      if (mapFields[p1]) {
+        const config = mapFields[p1]
+        text = getFormattedValue(text, config.format)
+      }
       return text
     })
     return formattedText
