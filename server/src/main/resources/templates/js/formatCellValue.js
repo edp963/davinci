111053c490078adcac5343e2558fcec1e3517d2b
shanmengm <mengmengshan@creditease.cn>
2019-06-20 11:18:17 +0800

    fix excel data out of order

--- server/src/main/resources/templates/js/formatCellValue.js
+++ server/src/main/resources/templates/js/formatCellValue.js
@@ -4534,6 +4534,54 @@ var global = {}
 
 })));
 // #endregion
+
+// #region polyfill
+// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
+if (!Array.prototype.findIndex) {
+    Object.defineProperty(Array.prototype, 'findIndex', {
+        value: function (predicate) {
+            // 1. Let O be ? ToObject(this value).
+            if (this == null) {
+                throw new TypeError('"this" is null or not defined');
+            }
+
+            var o = Object(this);
+
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
+
+            // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+            if (typeof predicate !== 'function') {
+                throw new TypeError('predicate must be a function');
+            }
+
+            // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+            var thisArg = arguments[1];
+
+            // 5. Let k be 0.
+            var k = 0;
+
+            // 6. Repeat, while k < len
+            while (k < len) {
+                // a. Let Pk be ! ToString(k).
+                // b. Let kValue be ? Get(O, Pk).
+                // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+                // d. If testResult is true, return k.
+                var kValue = o[k];
+                if (predicate.call(thisArg, kValue, k, o)) {
+                    return k;
+                }
+                // e. Increase k by 1.
+                k++;
+            }
+
+            // 7. Return -1.
+            return -1;
+        }
+    });
+}
+// #endregion
+
 // console.log(global.moment().format('YYYY-MM-DD'))
 // #region Tools
 var DEFAULT_SPLITER = '@davinci@'
@@ -4916,6 +4964,19 @@ function getCells(cfg, row, col, rowsLeft, fields, queryVars) {
     if (!cfg.isGroup || !cfg.children.length) {
         return cells
     }
+
+    cfg.children.sort(function (cfg1, cfg2) {
+        if (cfg1.isGroup || cfg2.isGroup) {
+            return 0
+        }
+        var cfg1Idx = fields.findIndex(function (f) {
+            return f.name === cfg1.headerName
+        })
+        var cfg2Idx = fields.findIndex(function (f) {
+            return f.name === cfg2.headerName
+        })
+        return cfg1Idx - cfg2Idx
+    })
 
     for (var i = 0; i < cfg.children.length; i++) {
         cells = cells.concat(getCells(cfg.children[i], row + rootRows, col, rowsLeft - rootRows, fields, queryVars))
