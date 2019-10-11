65574be06805e9878ccca562488c56eebffa7095
RichardShan <mengm.shan@gmail.com>
2019-10-11 22:25:49 +0800

    fix: check prefix and suffix in load custom datasource config

--- server/src/main/java/edp/core/utils/CustomDataSourceUtils.java
+++ server/src/main/java/edp/core/utils/CustomDataSourceUtils.java
@@ -29,7 +29,10 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static edp.core.consts.Consts.JDBC_DATASOURCE_DEFAULT_VERSION;
 
@@ -55,11 +58,11 @@ public class CustomDataSourceUtils {
 
 
     public static void loadAllFromYaml(String yamlPath) throws Exception {
-        
+
         if (StringUtils.isEmpty(yamlPath)) {
             return;
         }
-        
+
         File yamlFile = new File(yamlPath);
         if (!yamlFile.exists() || !yamlFile.isFile() || !yamlFile.canRead()) {
             return;
@@ -67,25 +70,25 @@ public class CustomDataSourceUtils {
 
         Yaml yaml = new Yaml();
         HashMap<String, Object> loads = yaml.loadAs(new BufferedReader(new FileReader(yamlFile)), HashMap.class);
-        
-        if(CollectionUtils.isEmpty(loads)) {
+
+        if (CollectionUtils.isEmpty(loads)) {
             return;
         }
-        
+
         ObjectMapper mapper = new ObjectMapper();
-        
+
         for (Map.Entry<String, Object> entry : loads.entrySet()) {
-            
+
             CustomDataSource customDataSource = mapper.convertValue(entry.getValue(), CustomDataSource.class);
-            
+
             if (StringUtils.isEmpty(customDataSource.getName()) || StringUtils.isEmpty(customDataSource.getDriver())) {
                 throw new Exception("Load custom datasource error: name or driver cannot be EMPTY");
             }
-            
+
             if ("null".equals(customDataSource.getName().trim().toLowerCase())) {
                 throw new Exception("Load custom datasource error: invalid name");
             }
-            
+
             if ("null".equals(customDataSource.getDriver().trim().toLowerCase())) {
                 throw new Exception("Load custom datasource error: invalid driver");
             }
@@ -93,17 +96,21 @@ public class CustomDataSourceUtils {
             if (StringUtils.isEmpty(customDataSource.getDesc())) {
                 customDataSource.setDesc(customDataSource.getName());
             }
-            
+
             if ("null".equals(customDataSource.getDesc().trim().toLowerCase())) {
                 customDataSource.setDesc(customDataSource.getName());
             }
 
-            if (StringUtils.isEmpty(customDataSource.getKeyword_prefix()) || StringUtils.isEmpty(customDataSource.getKeyword_suffix())) {
-                throw new Exception("Load custom datasource error: keyword prefixes and suffixes must be configured in pairs.");
+            if (!StringUtils.isEmpty(customDataSource.getKeyword_prefix()) || !StringUtils.isEmpty(customDataSource.getKeyword_suffix())) {
+                if (StringUtils.isEmpty(customDataSource.getKeyword_prefix()) || StringUtils.isEmpty(customDataSource.getKeyword_suffix())) {
+                    throw new Exception("Load custom datasource error: keyword prefixes and suffixes must be configured in pairs.");
+                }
             }
 
-            if (StringUtils.isEmpty(customDataSource.getAlias_prefix()) || StringUtils.isEmpty(customDataSource.getAlias_suffix())) {
-                throw new Exception("Load custom datasource error: alias prefixes and suffixes must be configured in pairs.");
+            if (!StringUtils.isEmpty(customDataSource.getAlias_prefix()) || !StringUtils.isEmpty(customDataSource.getAlias_suffix())) {
+                if (StringUtils.isEmpty(customDataSource.getAlias_prefix()) || StringUtils.isEmpty(customDataSource.getAlias_suffix())) {
+                    throw new Exception("Load custom datasource error: alias prefixes and suffixes must be configured in pairs.");
+                }
             }
 
             List<String> versoins = null;
