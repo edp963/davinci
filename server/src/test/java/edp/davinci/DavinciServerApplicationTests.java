92d4a67a2c5ed601429c5f164a01b9a1ea872e10
kevin.wu <kevin.wu>
2020-02-02 15:55:26 +0800

    no message

--- server/src/test/java/edp/davinci/DavinciServerApplicationTests.java
+++ server/src/test/java/edp/davinci/DavinciServerApplicationTests.java
@@ -22,7 +22,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.StringUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
