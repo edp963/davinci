27a9440aaca38c9c3b877dbce7af153e80b693a2
andyfong <fangkun1991s@163.com>
2020-06-05 14:08:23 +0800

    chore: update babel config and pkgs with ie11 compatibility

--- webapp/app/app.tsx
+++ webapp/app/app.tsx
@@ -18,10 +18,6 @@
  * >>
  */
 
-import '@babel/polyfill'
-import 'url-search-params-polyfill'
-import 'array-flat-polyfill'
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
