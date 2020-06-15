1656f00488ef021890413a59e019f86535ad5053
ruanhan1988 <2856197796@qq.com>
2020-06-15 18:03:27 +0800

    fix(shareparam) #1796

--- webapp/share/containers/Dashboard/index.tsx
+++ webapp/share/containers/Dashboard/index.tsx
@@ -250,13 +250,20 @@ export class Share extends React.Component<IDashboardProps, IDashboardStates> {
 
   private deep_set(o, path, value) {
     let i = 0
+    const val = decodeURIComponent(value)
     for (; i < path.length - 1; i++) {
-      if (o[path[i]] === undefined) {
-        o[decodeURIComponent(path[i])] = path[i + 1].match(/^\d+$/) ? [] : {}
-      }
-      o = o[decodeURIComponent(path[i])]
+        if (o[path[i]] === undefined) {
+            o[decodeURIComponent(path[i])] = path[i + 1].match(/^\d+$/) ? [] : {}
+        }
+        o = o[decodeURIComponent(path[i])]
     }
-    o[decodeURIComponent(path[i])] = decodeURIComponent(value)
+    if (o[decodeURIComponent(path[i])] && o[decodeURIComponent(path[i])].length) {
+        if (!(o[decodeURIComponent(path[i])].includes(val))) {
+            o[decodeURIComponent(path[i])] = [val].concat( o[decodeURIComponent(path[i])])
+        }
+    } else {
+        o[decodeURIComponent(path[i])] = val
+    }
   }
 
   private initPolling = (token) => {
