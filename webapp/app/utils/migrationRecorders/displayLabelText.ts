5fc835f7d0d2f35c9693d74ac243db88bc34b3cc
zll <1060962187@qq.com>
2020-09-01 17:41:59 +0800

    feat: add migrationRecorders styles hook

--- webapp/app/utils/migrationRecorders/displayLabelText.ts
+++ webapp/app/utils/migrationRecorders/displayLabelText.ts
@@ -1,15 +1,16 @@
-import { ILayerParams, IRichTextConfig } from 'app/containers/Display/components/types'
+import { ILayerParams } from 'app/containers/Display/components/types'
 import { IMigrationRecorder } from '.'
 import { buildLabelText } from 'app/containers/Display/components/Layer/RichText/util'
+import { useRecorderLabelStyle } from 'app/containers/Display/components/Layer/RichText/hooks'
 
 const displayLabelText: IMigrationRecorder<ILayerParams> = {
   versions: ['beta6'],
   recorder: {
     beta6(layerParams) {
       const { richText } = layerParams as ILayerParams
+      const { innerStyles, wrapStyles } = useRecorderLabelStyle(layerParams)
       if(!richText){
-        const oldText: string = Reflect.get(layerParams,'contentText')
-        const newText = buildLabelText( { fontSize: 40 },  oldText)
+        const newText = buildLabelText(innerStyles, Reflect.get(layerParams,'contentText'), wrapStyles)
         const success = Reflect.set(layerParams, 'richText', newText)
         if(success){
           Reflect.deleteProperty(layerParams, 'contentText')
