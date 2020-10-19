c1531bcdb2da267a9a938bfff98523add7733632
ruanhan1988 <2856197796@qq.com>
2020-10-19 15:29:22 +0800

    fix:(test) update jest.config.js

--- webapp/jest.config.js
+++ webapp/jest.config.js
@@ -29,7 +29,8 @@ module.exports = {
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/mocks/image.js',
     '^app/(.*)$': '<rootDir>/app/$1',
-    '^test/(.*)$': '<rootDir>/test/$1'
+    '^test/(.*)$': '<rootDir>/test/$1',
+    '^libs/(.*)$': '<rootDir>/libs/$1'
   },
   setupFilesAfterEnv: [
     // '<rootDir>/test/utils/test-bundler.js',
