1b58623f1646d6eb0c82fa30997667d3fc78d739
Zclhlmgqzc <502950670@qq.com>
2020-11-11 11:51:09 +0800

    fix(Display): #2032

--- webapp/app/containers/Display/Preview/Reveal.tsx
+++ webapp/app/containers/Display/Preview/Reveal.tsx
@@ -24,7 +24,8 @@ import { makeSelectCurrentProject } from 'containers/Projects/selectors'
 import { makeSelectCurrentDisplay } from 'containers/Viz/selectors'
 
 import Reveal from 'reveal.js'
-import 'reveal.js/css/reveal.css'
+import 'reveal.js/dist/reveal.css'
+import RevealZoom from 'reveal.js/plugin/zoom/plugin'
 
 import Display from './Display'
 import { DefaultDisplayParams } from '../components/Setting/constants'
@@ -64,12 +65,7 @@ const DisplayReveal: React.FC = () => {
 
       viewDistance: 100,
 
-      dependencies: [
-        {
-          src: 'plugin/zoom-js/zoom.js',
-          async: true
-        }
-      ]
+      plugins: [RevealZoom]
     })
   }, [])
 
